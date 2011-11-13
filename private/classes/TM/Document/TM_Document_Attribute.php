ab8d616033efe59389f3a019af61fb0897fa683b
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2011-11-13 22:23:52 +0700

    Создание и наполнение классов Task User

--- private/classes/TM/Document/TM_Document_Attribute.php
+++ private/classes/TM/Document/TM_Document_Attribute.php
@@ -8,7 +8,7 @@ require_once 'Attribute/TM_Attribute_Attribute.php';
  * class TM_Document_Attribute
  * 
  */
-class TM_Document_Attribute extends TM_Attribute_AttribyteType    //WARNING: PHP5 does not support multiple inheritance but there is more than 1 superclass defined in your UML model!
+class TM_Document_Attribute extends TM_Attribute_Attribute
 {
 
     /** Aggregations: */
@@ -18,9 +18,77 @@ class TM_Document_Attribute extends TM_Attribute_AttribyteType    //WARNING: PHP
      /*** Attributes: ***/
 
 
+    public function insertToDb()
+    {
+        // TODO: Implement insertToDb() method.
+    }
 
+    /**
+     *
+     *
+     * @return
+     * @access public
+     */
+    public function updateToDb()
+    {
+        // TODO: Implement updateToDb() method.
+    }
 
+    /**
+     *
+     *
+     * @return
+     * @access public
+     */
+    public function deleteFromDB()
+    {
+        // TODO: Implement deleteFromDB() method.
+    }
 
+    /**
+     *
+     *
+     * @param Task::TM_Task_Task task
 
+     * @param string key
+
+     * @return Attribute::TM_Attribute_Attribute
+     * @static
+     * @access public
+     */
+    public static function getInstanceByKey($task, $key)
+    {
+        // TODO: Implement getInstanceByKey() method.
+    }
+
+    /**
+     *
+     *
+     * @param array values
+
+     * @param Task::TM_Task_Task task
+
+     * @return Attribute::TM_Attribute_Attribute
+     * @static
+     * @access public
+     */
+    public static function getInstanceByArray($values, $task)
+    {
+        // TODO: Implement getInstanceByArray() method.
+    }
+
+    /**
+     *
+     *
+     * @param Task::TM_Task_Task task
+
+     * @return array
+     * @static
+     * @access public
+     */
+    public static function getAllInstance($task)
+    {
+        // TODO: Implement getAllInstance() method.
+    }
 } // end of TM_Document_Attribute
 ?>
--- private/classes/TM/Task/TM_Task_Attribute.php
+++ private/classes/TM/Task/TM_Task_Attribute.php
@@ -2,7 +2,8 @@
 
 require_once 'Attribute/TM_Attribute_Attribute.php';
 require_once 'Task/TM_Task_Task.php';
-require_once 'array.php';
+
+//task_id, attribute_key, type_id, attribute_value
 
 
 /**
@@ -19,6 +20,10 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      /*** Attributes: ***/
 
 
+    public function __construct(TM_Task_Task $task) {
+       parent::__construct($task);
+    }
+
     /**
      * 
      *
@@ -26,6 +31,13 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      * @access public
      */
     public function insertToDb( ) {
+        try {
+            $sql = 'INSERT INTO tm_task_attribute(task_id, attribute_key, type_id, attribute_value)
+                    VALUES (' . $this->_task->getId() . ', "' . $this->_attribyteKey . '", ' . $this->_type->getId() . ', "' . $this->_value  . '")';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     } // end of member function insertToDb
 
     /**
@@ -35,6 +47,14 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      * @access public
      */
     public function updateToDb( ) {
+         try {
+            $sql = 'UPDATE tm_task_attribute
+                    SET type_id="' . $this->_type->getId() . '", attribute_value="' . $this->_value . '"
+                    WHERE task_id=' .  $this->_task->getId() . ' AND attribute_key="' . $this->_attribyteKey . '"';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     } // end of member function updateToDb
 
     /**
@@ -44,6 +64,13 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      * @access public
      */
     public function deleteFromDb( ) {
+        try {
+            $sql = 'DELETE FROM tm_task_attribute
+                    WHERE task_id=' .  $this->_task->getId() . ' AND attribute_key="' . $this->_attribyteKey . '"' ;
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     } // end of member function deleteFromDb
 
     /**
@@ -57,7 +84,22 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      * @static
      * @access public
      */
-    public static function getInstanceByKey( $task,  $key ) {
+    public static function getInstanceByKey(TM_Task_Task $task,  $key ) {
+         try {
+           $db = simo_db::getInstance();
+            $sql = 'SELECT * FROM tm_task_attribute WHERE task_id=' .  $task->getId() . ' AND attribute_key="' . $key . '"' ;
+            $result = $db->query($sql, simo_db::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $o = new TM_Task_Attribute($task);
+                $o->fillFromArray($result[0]);
+                return $o;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     } // end of member function getInstanceByKey
 
     /**
@@ -69,12 +111,47 @@ class TM_Task_Attribute extends TM_Attribute_Attribute
      * @static
      * @access public
      */
-    public static function getAllInstance( $task ) {
+    public static function getAllInstance(TM_Task_Task $task ) {
+        try {
+            $db = simo_db::getInstance();
+            $sql = 'SELECT * FROM tm_task_attribute WHERE task_id=' .  $task->getId();
+            $result = $db->query($sql, simo_db::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $retArray = array();
+                foreach ($result as $res) {
+                    $retArray[] = TM_Task_Attribute::getInstanceByArray($res, $task);
+                }
+                return $retArray;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     } // end of member function getAllInstance
 
 
+    /**
+     *
+     *
+     * @param array values
 
+     * @param Task::TM_Task_Task task
 
-
+     * @return Attribute::TM_Attribute_Attribute
+     * @static
+     * @access public
+     */
+    public static function getInstanceByArray($values, TM_Task_Task $task)
+    {
+        try {
+            $o = new TM_Task_Attribute($task);
+            $o->fillFromArray($values);
+            return $o;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
 } // end of TM_Task_Attribute
 ?>
