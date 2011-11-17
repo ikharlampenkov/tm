dbc1b39413f44b83dc566efc3508ff7206978425
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2011-11-17 21:20:38 +0700

    Добавлена поддержка ZendFramework и Smarty

--- runtime/templates_c/adce21d5e4bd44928444e8e78142d735bf514e9d.file.index.tpl.php
+++ runtime/templates_c/adce21d5e4bd44928444e8e78142d735bf514e9d.file.index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.0.9, created on 2011-11-15 20:39:33
+<?php /* Smarty version Smarty-3.0.9, created on 2011-11-17 21:11:21
          compiled from "F:\www\tm\application/views/scripts\user/index.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:196604ec26b9524bb06-69221057%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_smarty_tpl->decodeProperties(array (
     'adce21d5e4bd44928444e8e78142d735bf514e9d' => 
     array (
       0 => 'F:\\www\\tm\\application/views/scripts\\user/index.tpl',
-      1 => 1321363664,
+      1 => 1321379128,
       2 => 'file',
     ),
   ),
