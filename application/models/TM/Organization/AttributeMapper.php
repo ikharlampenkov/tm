34eaf624488388dc26c2f0ef3eb7a519d5d7f3ac
Ivan Kharlampenkov <ivan87kharlampenkov@gmail.com>
2012-03-13 23:44:48 +0700

    Классы для организации

---
+++ application/models/TM/Organization/AttributeMapper.php
@@ -0,0 +1,165 @@
+<?php
+
+//task_id, attribute_key, type_id, attribute_value
+
+
+/**
+ * class TM_Task_Attribute
+ *
+ */
+class TM_Organization_AttributeMapper extends TM_Attribute_AttributeMapper
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     *
+     *
+     * @param TM_Attribute_Attribute $attribute
+     * @return void
+     * @access public
+     */
+    public function insertToDb(TM_Attribute_Attribute $attribute)
+    {
+        try {
+            if (!empty($attribute->value)) {
+                $isFill = 1;
+            } else {
+                $isFill = 0;
+            }
+
+            $sql = 'REPLACE INTO tm_organization_attribute(organization_id, attribute_key, type_id, attribute_value, attribute_order, is_fill)
+                    VALUES (' . $attribute->task->getId() . ', "' . $attribute->attribyteKey . '", ' . $attribute->type->getId() . ',
+                           "' . $attribute->value . '", ' . $attribute->attributeOrder . ', ' . $isFill . ')';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function insertToDb
+
+    /**
+     *
+     * @param $attribute
+     * @return void
+     * @access public
+     */
+    public function updateToDb($attribute)
+    {
+        try {
+            if (!empty($attribute->value)) {
+                $isFill = 1;
+            } else {
+                $isFill = 0;
+            }
+
+            $sql = 'UPDATE tm_organization_attribute
+                    SET type_id="' . $attribute->type->getId() . '", attribute_value="' . $attribute->value . '",
+                        attribute_order=' . $attribute->attributeOrder . ', is_fill=' . $isFill . '
+                    WHERE organization_id=' . $attribute->task->getId() . ' AND attribute_key="' . $attribute->attribyteKey . '"';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function updateToDb
+
+    /**
+     *
+     *
+     * @return
+     * @access public
+     */
+    public function deleteFromDb($attribute)
+    {
+        try {
+            $sql = 'DELETE FROM tm_organization_attribute
+                    WHERE organization_id=' . $attribute->task->getId() . ' AND attribute_key="' . $attribute->attribyteKey . '"';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function deleteFromDb
+
+    /**
+     *
+     *
+     * @param $object
+
+     * @param string $key
+
+     * @return Attribute::TM_Attribute_Attribute
+     * @static
+     * @access public
+     */
+    public function getInstanceByKey($object, $key)
+    {
+        try {
+            $db = StdLib_DB::getInstance();
+            $sql = 'SELECT * FROM tm_organization_attribute WHERE organization_id=' . $object->getId() . ' AND attribute_key="' . $key . '"';
+            $result = $db->query($sql, StdLib_DB::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $o = new TM_Attribute_Attribute($object, $this);
+                $o->fillFromArray($result[0]);
+                return $o;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function getInstanceByKey
+
+    /**
+     *
+     *
+     * @param $object
+
+     * @return array
+     * @static
+     * @access public
+     */
+    public function getAllInstance($object)
+    {
+        try {
+            $db = StdLib_DB::getInstance();
+            $sql = 'SELECT * FROM tm_organization_attribute WHERE organization_id=' . $object->getId();
+            $result = $db->query($sql, StdLib_DB::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $retArray = array();
+                foreach ($result as $res) {
+                    $retArray[] = TM_Attribute_Attribute::getInstanceByArray($this, $object, $res);
+                }
+                return $retArray;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function getAllInstance
+
+
+    /**
+     *
+     * @param $object
+     * @param array $values
+     *
+     * @return TM_Attribute_Attribute
+     * @access public
+     */
+    public function getInstanceByArray($object, $values)
+    {
+        try {
+            $o = new TM_Attribute_Attribute($this, $object);
+            $o->fillFromArray($values);
+            return $o;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+} // end of TM_Organization_Attribute
+?>
---
+++ application/models/TM/Organization/Hash.php
@@ -0,0 +1,467 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Moris
+ * Date: 19.11.11
+ * Time: 14:38
+ * To change this template use File | Settings | File Templates.
+ */
+
+class TM_Organization_Hash
+{
+
+    /**
+     *
+     * @access protected
+     */
+    protected $_task;
+
+    /**
+     *
+     * @access protected
+     */
+    protected $_attributeKey = '';
+
+    /**
+     *
+     * @access protected
+     */
+    protected $_title;
+
+    /**
+     *
+     * @var TM_Attribute_AttributeType
+     * @access protected
+     */
+    protected $_type = null;
+
+    /**
+     * @var string
+     */
+    protected $_listValue = '';
+
+    /**
+     * @var string
+     */
+    protected $_listOrder = '';
+
+    /**
+     * @var bool
+     */
+    protected $_isRequired = false;
+
+    /**
+     * @var int
+     */
+    protected $_sortOrder = 1000;
+
+    /**
+     *
+     * @access protected
+     */
+    protected $_db;
+
+    /**
+     *
+     *
+     * @return TM_Organization_Organization
+     * @access public
+     */
+    public function getOrganization()
+    {
+        return $this->_task;
+    } // end of member function getOrganization
+
+    /**
+     *
+     *
+     * @return Attribute::TM_Attribute_AttribyteType
+     * @access public
+     */
+    public function getType()
+    {
+        return $this->_type;
+    } // end of member function getType
+
+    /**
+     *
+     *
+     * @return string
+     * @access public
+     */
+    public function getAttributeKey()
+    {
+        return $this->_attributeKey;
+    } // end of member function getAttribyteKey
+
+    public function setAttributeKey($value)
+    {
+        $this->_attributeKey = $this->_db->prepareString($value);
+    }
+
+    /**
+     *
+     *
+     * @param TM_Organization_Organization $value
+     * @return void
+     * @access protected
+     */
+    protected function setOrganization(TM_Organization_Organization $value)
+    {
+        $this->_task = $value;
+
+    } // end of member function setOrganization
+
+    /**
+     *
+     *
+     * @return string
+     * @access public
+     */
+    public function getTitle()
+    {
+        return $this->_db->prepareStringToOut($this->_title);
+    } // end of member function getTitle
+
+    /**
+     *
+     *
+     * @param TM_Attribute_AttributeType $value
+     * @return void
+     * @access protected
+     */
+    public function setType(TM_Attribute_AttributeType $value)
+    {
+        $this->_type = $value;
+    } // end of member function setType
+
+    /**
+     *
+     *
+     * @param string $value
+     * @return void
+     * @access public
+     */
+    public function setTitle($value)
+    {
+        $this->_title = $this->_db->prepareString($value);
+    } // end of member function setTitle
+
+    /**
+     *
+     *
+     * @param array|string $value
+     * @return void
+     * @access public
+     */
+    public function setValueList($value)
+    {
+        if (is_array($value)) {
+            $this->_listValue = $this->_db->prepareString(implode('||', $value));
+        } else {
+            $this->_listValue = $this->_db->prepareString(trim($value));
+        }
+    } // end of member function setValueList
+
+    /**
+     *
+     *
+     * @param bool $asString
+     * @return array|string
+     * @access public
+     */
+    public function getValueList($asString = false, $isClear = false)
+    {
+        if ($asString) {
+            return $this->_listValue;
+        } else {
+            if ($isClear) {
+                $temp = str_replace('*', '', $this->_listValue);
+            } else {
+                $temp = $this->_listValue;
+            }
+            return explode('||', $this->_db->prepareStringToOut($temp));
+        }
+
+    } // end of member function getValueList
+
+    /**
+     * @param string $listOrder
+     */
+    public function setListOrder($listOrder)
+    {
+        if (is_array($listOrder)) {
+            $this->_listOrder = $this->_db->prepareString(implode('||', $listOrder));
+        } else {
+            $this->_listOrder = $this->_db->prepareString(trim($listOrder));
+        }
+    }
+
+    /**
+     * @param bool $asString
+     * @return array|string
+     * @return string
+     */
+    public function getListOrder($asString = false)
+    {
+        if ($asString) {
+            return $this->_listOrder;
+        } else {
+            return explode('||', $this->_db->prepareStringToOut($this->_listOrder));
+        }
+    }
+
+
+    /**
+     * @param boolean $isRequired
+     */
+    public function setIsRequired($isRequired)
+    {
+        if ($isRequired === 'on') {
+            $this->_isRequired = 1;
+        } elseif (empty($isRequired)) {
+            $this->_isRequired = 0;
+        } else {
+            $this->_isRequired = $isRequired;
+        }
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getIsRequired()
+    {
+        return $this->_isRequired;
+    }
+
+    /**
+     * @param int $sortOrder
+     */
+    public function setSortOrder($sortOrder)
+    {
+        $this->_sortOrder = $sortOrder;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSortOrder()
+    {
+        return $this->_sortOrder;
+    } // end of member function fillFromArray
+
+    protected function _prepareBool($value)
+    {
+        if ($value) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $method = "get{$name}";
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    /**
+     *
+     *
+     * @return TM_Organization_Hash
+     * @access public
+     */
+    public function __construct()
+    {
+        $this->_db = StdLib_DB::getInstance();
+    } // end of member function __construct
+
+    /**
+     *
+     *
+     * @return void
+     * @access public
+     */
+    public function insertToDb()
+    {
+        try {
+            $sql = 'INSERT INTO tm_task_hash(task_id, attribute_key, title, type_id, list_value, list_order, required, sort_order)
+                    VALUES (NULL, "' . $this->_attributeKey . '", "' . $this->_listOrder . '", "' . $this->_title . '", ' . $this->_type->getId() . ',
+                            "' . $this->_listValue . ' ",  ' . $this->_prepareBool($this->_isRequired) . ', ' . $this->_sortOrder . ')';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function insertToDb
+
+    //task_id, attribute_key, title, type_id, required, sort_order
+
+    /**
+     *
+     *
+     * @return void
+     * @access public
+     */
+    public function updateToDb()
+    {
+        try {
+            $sql = 'UPDATE tm_task_hash
+                    SET title="' . $this->_title . '", type_id=' . $this->_type->getId() . ',
+                        list_value="' . $this->_listValue . ' ", list_order="' . $this->_listOrder . '",
+                        required=' . $this->_prepareBool($this->_isRequired) . ', sort_order=' . $this->_sortOrder . '
+                    WHERE task_id IS NULL AND attribute_key="' . $this->_attributeKey . '"';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function updateToDb
+
+    /**
+     *
+     *
+     * @return void
+     * @access public
+     */
+    public function deleteFromDb()
+    {
+        try {
+            $sql = 'DELETE FROM tm_task_hash
+                    WHERE task_id IS NULL AND attribute_key="' . $this->_attributeKey . '"';
+            $this->_db->query($sql);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function deleteFromDb
+
+    /**
+     *
+     *
+     * @param int $key идентификатор задачи
+     * @return TM_Organization_Hash
+     * @static
+     * @access public
+     */
+    public static function getInstanceById($key)
+    {
+        try {
+            $db = StdLib_DB::getInstance();
+            $sql = 'SELECT * FROM tm_task_hash WHERE task_id IS NULL AND attribute_key="' . $key . '"';
+            $result = $db->query($sql, StdLib_DB::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $o = new TM_Organization_Hash();
+                $o->fillFromArray($result[0]);
+                return $o;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function getInstanceById
+
+    /**
+     *
+     *
+     * @param array $values
+     * @return TM_Organization_Hash
+     * @static
+     * @access public
+     */
+    public static function getInstanceByArray($values)
+    {
+        try {
+            $o = new TM_Organization_Hash();
+            $o->fillFromArray($values);
+            return $o;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function getInstanceByArray
+
+    /**
+     *
+     * @param $object
+     * @return array
+     * @static
+     * @access public
+     */
+    public static function getAllInstance($object = null)
+    {
+        try {
+            $db = StdLib_DB::getInstance();
+
+            /*
+            if (!is_null($object)) {
+                $sql = 'SELECT COUNT(attribute_key) FROM tm_task_attribute WHERE task_id=' . $object->id;
+                $result = $db->query($sql, StdLib_DB::QUERY_MODE_NUM);
+            }
+
+            $sql = 'SELECT tm_task_hash.attribute_key, title, tm_task_hash.type_id, list_value, required, sort_order  FROM tm_task_hash';
+
+            if (!is_null($object) && isset($result[0][0]) && $result[0][0] > 0) {
+                $sql .= ' LEFT JOIN tm_task_attribute ON tm_task_hash.attribute_key=tm_task_attribute.attribute_key
+                          WHERE tm_task_attribute.task_id=' . $object->id . ' 
+                          ORDER BY required DESC, sort_order, is_fill DESC, title';
+            } else {
+                $sql .= ' ORDER BY required DESC, sort_order, title';
+            }
+
+            print_r($sql);
+            */
+
+            $sql = 'SELECT tm_task_hash.attribute_key, title, tm_task_hash.type_id, list_value, list_order, required, sort_order
+                    FROM tm_task_hash ';
+
+            if (!is_null($object)) {
+                $sql .= ' LEFT JOIN (
+                        SELECT * FROM tm_task_attribute WHERE tm_task_attribute.task_id=' . $object->id . '
+                    ) t2 ON tm_task_hash.attribute_key=t2.attribute_key
+                    ORDER BY required DESC, sort_order, title';
+            } else {
+                $sql .= ' ORDER BY required DESC, sort_order, title';
+            }
+
+            $result = $db->query($sql, StdLib_DB::QUERY_MOD_ASSOC);
+
+            if (isset($result[0])) {
+                $retArray = array();
+                foreach ($result as $res) {
+                    $retArray[] = TM_Organization_Hash::getInstanceByArray($res);
+                }
+                return $retArray;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    } // end of member function getAllInstance
+
+
+    /**
+     *
+     *
+     * @param array $values
+     * @return void
+     * @access public
+     */
+    public function fillFromArray($values)
+    {
+        //$this->setOrganization($values['task_id']);
+        $this->setAttributeKey($values['attribute_key']);
+        $this->setTitle($values['title']);
+
+        $this->setType(TM_Attribute_AttributeTypeFactory::getAttributeTypeById(new TM_Organization_AttributeTypeMapper(), $values['type_id']));
+        $this->setValueList($values['list_value']);
+        $this->setListOrder($values['list_order']);
+        $this->setIsRequired($values['required']);
+        $this->setSortOrder($values['sort_order']);
+    }
+}
